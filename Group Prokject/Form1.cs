a58f5789d884f04198b5aa2e125397c86e4151f6
Christopher Albers-Hardebeck <calber32@thomasmore.edu>
2020-09-23 16:27:33 -0400

    Added Master Button and Picture Box

--- Group Prokject/Form1.cs
+++ Group Prokject/Form1.cs
@@ -16,5 +16,15 @@ namespace Group_Prokject
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+        }
     }
 }
